43b5b84236f2e249fe0e20d4e15442f7e217920e
Eckson Glitho <g.eckson03@gmail.com>
2022-06-13 23:22:22 +0000

    bloggy sql maj

--- bloggy.sql
+++ bloggy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 13 juin 2022 à 03:23
+-- Généré le : mar. 14 juin 2022 à 01:21
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.1.2
 
@@ -34,15 +34,6 @@ CREATE TABLE `post` (
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `post`
---
-
-INSERT INTO `post` (`Id`, `title`, `description`, `content`) VALUES
-(1, 'khjdvjk b', 'lalalalalallalalalal', '55555'),
-(2, 'laravel', 'php', 'alou aime php'),
-(3, '11111111', 'jgvcjhvbfj', '2222222');
-
 -- --------------------------------------------------------
 
 --
@@ -55,13 +46,6 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`Id`, `fullname`, `email`, `password`) VALUES
-(1, 'blogger@gmail.com', 'blogger@gmail.com', 'bloggy');
 
 --
 -- Index pour les tables déchargées
@@ -87,13 +71,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
